32c0b6133c3520d450988cba68c39377d5a4a371
Mohammed <najjarmedd@gmail.com>
2022-10-26 07:32:56 +0100

    add tailwaindcss

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>welcome home</h1>   
+      <h1 className='text-6xl font-serif text-center'>welcome home</h1>   
     </div>
   );
 }
