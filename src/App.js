92bade7dc57a3a4c6c436517f46bb4de94f868b3
Mohammed <najjarmedd@gmail.com>
2022-10-26 07:59:32 +0100

    add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import {suitekitchen} from './assets/index'
 
 function App() {
   return (
-    <div>
-      <h1 className='text-6xl font-serif text-center'>welcome home</h1>   
+    <div className='max-w-7xl mx-auto py-4'>
+      <div className='flex justify-between items-center border-y border-black py-5 px-4'>
+        <div className='flex justify-around items-center'>
+          <img className='h-20' src={suitekitchen} alt="logo suite kitchen"/>
+          <h3 className='hidden md:inline-flex text-3xl font-serif'>Suite Kitchen</h3>
+        </div>
+        <div>
+          <h3 className='text-2xl font-bold'>+22230411047 </h3>
+        </div>
+
+      </div>
+      <h1 className='text-6xl font-serif text-center mt-4'>welcome home</h1>   
     </div>
   );
 }
---
+++ src/assets/index.js
@@ -0,0 +1,23 @@
+import sk from './sk.jpg'
+import sk2 from './sk2.jpg'
+import sk3 from './sk3.jpg'
+import sk4 from './sk4.jpg'
+import sk5 from './sk5.jpg'
+import sk6 from './sk6.jpg'
+import sk7 from './sk7.jpg'
+import suitekitchen from './suitekitchen.png'
+import wtsap from './whatsapp.png'
+
+
+
+export{
+    sk,
+    sk2,
+    sk3,
+    sk4,
+    sk5,
+    sk6,
+    sk7,
+    suitekitchen,
+    wtsap
+}
