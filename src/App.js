71d95d27f41f3e266125bb82b7f91d29712fa314
Mohammed Najjar <57860863+mednajjar@users.noreply.github.com>
2024-01-12 01:20:21 +0100

    Update App.js
    add chat pluging

--- src/App.js
+++ src/App.js
@@ -133,6 +133,36 @@ function App() {
         </Swiper>
         {/* </Grid> */}
       </Grid>
+      <!-- Messenger Chat Plugin Code -->
+    <div id="fb-root"></div>
+
+    <!-- Your Chat Plugin code -->
+    <div id="fb-customer-chat" class="fb-customerchat">
+    </div>
+
+    <script>
+      var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "107570595483443");
+      chatbox.setAttribute("attribution", "biz_inbox");
+    </script>
+
+    <!-- Your SDK code -->
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v18.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/ar_AR/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    </script>
       <div className='flex flex-col text-center items-center border my-8 mx-3 p-5 shadow-xl'>
 
         <h2 className='text-xl my-2'>
